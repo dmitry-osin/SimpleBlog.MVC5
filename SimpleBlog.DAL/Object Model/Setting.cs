5ca2b83b14710865a630694b1c4d6f0c0ae1e0d9
Dmitry Osin <dmitry@osin.pw>
2016-01-21 03:51:51 +0300

    Фиксы настроек, добавил файл Settings.config

--- SimpleBlog.DAL/Object Model/Setting.cs
+++ SimpleBlog.DAL/Object Model/Setting.cs
@@ -2,6 +2,7 @@
 {
     public class Setting
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Type { get; set; }
         public string Value { get; set; } 

c0407e714fdf93bb12eadddb66bbd3d199cc55c3
Dmitry Osin <dmitry@osin.pw>
2016-01-21 23:41:56 +0300

    Фиксы

---
+++ SimpleBlog.WebUI/Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SimpleBlog.WebUI.Areas.Admin.Controllers
+{
+    [Authorize]
+    public class AdminController : Controller
+    {
+        // GET: Admin/Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

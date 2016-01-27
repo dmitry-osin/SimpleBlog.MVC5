bc403e0d14396b7ccf2af5548d1c58ded3152a83
Dmitry Osin <dmitry@osin.pw>
2016-01-27 20:30:54 +0300

    Добавил AppSettingsStore

---
+++ SimpleBlog.WebUI/Utils/AppSettingsStore.cs
@@ -0,0 +1,55 @@
+﻿using SimpleBlog.DAL.Context;
+using SimpleBlog.DAL.DataService;
+using SimpleBlog.DAL.Object_Model;
+
+namespace SimpleBlog.WebUI.Utils
+{
+    public class AppSettingsStore
+    {
+        private UnitOfWork _unitOfWork;
+
+        public AppSettingsStore()
+        {
+            _unitOfWork = new UnitOfWork(ApplicationContext.Create());
+        }
+
+        public Setting DateFormat
+        {
+            get { return _unitOfWork.Settings.GetByName("DateFormat"); }
+            set
+            {
+                _unitOfWork.Settings.Update(value);
+            }
+        }
+
+
+        public Setting BlogName
+        {
+            get { return _unitOfWork.Settings.GetByName("BlogName"); }
+            set
+            {
+                _unitOfWork.Settings.Update(value);
+            }
+        }
+
+
+        public Setting PersistenceTimeOfAuth
+        {
+            get { return _unitOfWork.Settings.GetByName("PersistenceTimeOfAuth"); }
+            set
+            {
+                _unitOfWork.Settings.Update(value);
+            }
+        }
+
+
+        public Setting PostsPerPage
+        {
+            get { return _unitOfWork.Settings.GetByName("PostsPerPage"); }
+            set
+            {
+                _unitOfWork.Settings.Update(value);
+            }
+        }
+    }
+}

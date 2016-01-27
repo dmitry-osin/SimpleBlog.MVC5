524e0ae631cdf693d1e86933c8c29f217cc20a2c
Dmitry Osin <dmitry@osin.pw>
2016-01-27 20:50:56 +0300

    Добавил SettingController и методы List и Edit

---
+++ SimpleBlog.WebUI/Areas/Admin/Controllers/SettingController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SimpleBlog.DAL.Context;
+using SimpleBlog.DAL.DataService;
+using SimpleBlog.DAL.Object_Model;
+
+namespace SimpleBlog.WebUI.Areas.Admin.Controllers
+{
+    [Authorize]
+    public class SettingController : Controller
+    {
+        private UnitOfWork _unitOfWork;
+
+        public SettingController()
+        {
+            _unitOfWork = new UnitOfWork(ApplicationContext.Create());
+        }
+
+        // GET: Admin/Setting
+        public ActionResult List()
+        {
+            return View(_unitOfWork.Settings.GetAll());
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            return View(_unitOfWork.Settings.GetById(id));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Setting model)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Settings.Update(model);
+            }
+            return View();
+        }
+    }
+}

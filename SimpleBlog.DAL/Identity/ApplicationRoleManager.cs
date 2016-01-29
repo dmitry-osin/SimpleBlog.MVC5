e7e46e4318e009b80468936b706137293f70794f
Dmitry Osin <dmitry@osin.pw>
2016-01-29 16:59:09 +0300

    Добавил ApplicationRoleManager

---
+++ SimpleBlog.DAL/Identity/ApplicationRoleManager.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using SimpleBlog.DAL.Context;
+using SimpleBlog.DAL.Object_Model;
+
+namespace SimpleBlog.DAL.Identity
+{
+    public class ApplicationRoleManager : RoleManager<ApplicationRole>
+    {
+        public ApplicationRoleManager(RoleStore<ApplicationRole> store)
+            : base(store)
+        { }
+
+        public static ApplicationRoleManager Create(IdentityFactoryOptions<ApplicationRoleManager> options,
+                                            IOwinContext context)
+        {
+            return new ApplicationRoleManager(new
+                    RoleStore<ApplicationRole>(context.Get<ApplicationContext>()));
+        }
+    }
+}

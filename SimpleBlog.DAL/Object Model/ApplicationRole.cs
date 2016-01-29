ce7540c868b08eae0e944b9291367437ec96fb51
Dmitry Osin <dmitry@osin.pw>
2016-01-29 16:57:43 +0300

    Добавить ApplicationRole

---
+++ SimpleBlog.DAL/Object Model/ApplicationRole.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace SimpleBlog.DAL.Object_Model
+{
+    public class ApplicationRole : IdentityRole
+    {
+        public ApplicationRole() { }
+
+        public string Description { get; set; } 
+    }
+}
